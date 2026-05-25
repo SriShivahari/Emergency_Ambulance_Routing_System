89276d4ccde065a463af103e703c13fff9d0e25f
Sri Shivahari Kannan <112324724+SriShivahari@users.noreply.github.com>
2026-05-25 13:19:03 +0530

    Update Google Maps API key to use a placeholder
    Replaced the hardcoded Google Maps API key with a placeholder.

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-    GOOGLE_MAPS_API_KEY ="AIzaSyALr0rte2iS7R0NObUQXDFqtChVHC4DOYA"
+    GOOGLE_MAPS_API_KEY = YOUR API KEY
